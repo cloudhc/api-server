b2472d1a1eaf2e18541b582905f75b93cfe0aeaf
bast <cloudhc@naver.com>
2016-04-19 17:54:00 +0900

    add sql files

---
+++ insert-table.sql
@@ -0,0 +1,5 @@
+INSERT INTO "NETTY_EXAMPLE_DB"."USERS" ("USERID", "USERNAME", "PASSWORD") VALUES('foo@foobar.com', 'foobar', 'passWords');
+INSERT INTO "NETTY_EXAMPLE_DB"."USERS" ("USERID", "USERNAME", "PASSWORD") VALUES('kris@foobar.com', 'kris jeong', 'passWords');
+INSERT INTO "NETTY_EXAMPLE_DB"."USERS" ("USERID", "USERNAME", "PASSWORD") VALUES('ace@foobar.com', '김일등', 'passWords');
+INSERT INTO "NETTY_EXAMPLE_DB"."USERS" ("USERID", "USERNAME", "PASSWORD") VALUES('amy@foobar.com', 'amy lim', '');
+INSERT INTO "NETTY_EXAMPLE_DB"."USERS" ("USERID", "USERNAME", "PASSWORD") VALUES('emma@foobar.com', 'emma', 'emma');
